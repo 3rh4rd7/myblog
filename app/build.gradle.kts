280aece776af01dfb3fd0ef34273da6d684fe14b
Dmitrii Pochtennov <dmitrii.pochtennov@personio.de>
2025-04-06 16:44:15 +0200

    [FIRST_HOMEWORK] add first controller and set up web application context

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,18 @@ val springFrameworkVersion = "6.2.5"
 val junitJupiterVersion = "5.12.1"
 val springDataVersion = "3.4.2"
 val thymeleafVersion = "3.1.3.RELEASE"
+val postgresqlVersion = "42.7.5"
+val jooqVersion = "3.20.3"
+val flywayCore = "11.6.0"
+val jakartaServletApiVersion = "6.1.0"
+val hamcrestVersion = "3.0"
+
+group = "org.myblog"
+version = "0.0.1-SNAPSHOT"
 
 plugins {
     java
-    application
-    id("io.freefair.lombok") version "8.13.1"
+    war
 }
 
 repositories {
@@ -14,33 +21,32 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-context:$springFrameworkVersion")
     implementation("org.springframework:spring-webmvc:$springFrameworkVersion")
     implementation("org.springframework.data:spring-data-jdbc:$springDataVersion")
+
+    implementation("org.flywaydb:flyway-core:$flywayCore")
+    implementation("org.jooq:jooq:$jooqVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
 
     implementation("org.thymeleaf:thymeleaf:$thymeleafVersion")
     implementation("org.thymeleaf:thymeleaf-spring6:$thymeleafVersion")
 
+    implementation("jakarta.servlet:jakarta.servlet-api:$jakartaServletApiVersion")
+
     testImplementation(platform("org.junit:junit-bom:$junitJupiterVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.hamcrest:hamcrest:$hamcrestVersion")
 
     testImplementation("org.springframework:spring-test:$springFrameworkVersion")
 }
 
 tasks.test {
     useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
 }
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
-}
-
-application {
-    mainClass = "org.myblog.App"
 }
---
+++ app/src/main/java/org/myblog/api/http/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.myblog.api.http;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
